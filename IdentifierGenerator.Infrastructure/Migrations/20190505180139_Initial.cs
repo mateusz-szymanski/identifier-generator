731b2c3e62f03a31e3e9ea3bf790bc5c7e67bd6d
Mateusz Szymański <mateuszd.szymanski@gmail.com>
2019-05-05 20:07:05 +0200

    Add WebApi project with docker and linux mssql

---
+++ IdentifierGenerator.Infrastructure/Migrations/20190505180139_Initial.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentifierGenerator.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Identifier",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    GlobalId = table.Column<Guid>(nullable: false),
+                    Value = table.Column<int>(nullable: false),
+                    FactoryCode = table.Column<string>(maxLength: 30, nullable: true),
+                    CategoryCode = table.Column<string>(maxLength: 30, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Identifier", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IdentifierGenerated",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IdentifierGlobalId = table.Column<Guid>(nullable: false),
+                    Code = table.Column<string>(nullable: true),
+                    GeneratedOn = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdentifierGenerated", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Identifier_GlobalId",
+                table: "Identifier",
+                column: "GlobalId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Identifier_FactoryCode_CategoryCode",
+                table: "Identifier",
+                columns: new[] { "FactoryCode", "CategoryCode" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdentifierGenerated_IdentifierGlobalId",
+                table: "IdentifierGenerated",
+                column: "IdentifierGlobalId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Identifier");
+
+            migrationBuilder.DropTable(
+                name: "IdentifierGenerated");
+        }
+    }
+}
---
+++ IdentifierGenerator.WebApi/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentifierGenerator.Bootstrap;
+using IdentifierGenerator.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace IdentifierGenerator.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Bootstrapper.ConfigureServices(services, Configuration.GetConnectionString("IdentifierGeneratorContext"));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<IdentifierGeneratorDbContext>();
+                context.Database.Migrate();
+            }
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
