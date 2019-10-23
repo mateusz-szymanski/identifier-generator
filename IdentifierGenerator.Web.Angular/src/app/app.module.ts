11ecd84e91b5b2cfb3369fbde9895810c2819725
Mateusz Szymański <mateuszd.szymanski@gmail.com>
2019-10-23 18:20:46 +0200

    Move shared declarations to shared module

--- IdentifierGenerator.Web.Angular/src/app/app.module.ts
+++ IdentifierGenerator.Web.Angular/src/app/app.module.ts
@@ -1,19 +1,11 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 import { HomeScreenModule } from './home-screen/home-screen.module';
 import { IdentifierScreenModule } from './identifier-screen/identifier-screen.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -21,12 +13,6 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     PageNotFoundComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatDividerModule,
     SharedModule,
     HomeScreenModule,
     IdentifierScreenModule,
--- IdentifierGenerator.Web.Angular/src/app/shared/shared.module.ts
+++ IdentifierGenerator.Web.Angular/src/app/shared/shared.module.ts
@@ -1,8 +1,13 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoadingOverlayComponent } from './loading-overlay/loading-overlay.component';
-
 
 @NgModule({
   declarations: [
@@ -10,10 +15,21 @@ import { LoadingOverlayComponent } from './loading-overlay/loading-overlay.compo
   ],
   imports: [
     CommonModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDividerModule
   ],
   exports: [
-    LoadingOverlayComponent
-  ]
+    LoadingOverlayComponent,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatDividerModule]
 })
 export class SharedModule { }
